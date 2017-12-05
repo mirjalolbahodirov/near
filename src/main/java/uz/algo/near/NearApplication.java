d6a56bcf1330fe839e08c64a78b77da76a4497be
mirjalolbahodirov <mirjalol.bahodirov@gmail.com>
2017-12-06 03:29:29 +0500

    Initial Commit

---
+++ src/main/java/uz/algo/near/NearApplication.java
@@ -0,0 +1,12 @@
+package uz.algo.near;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearApplication.class, args);
+	}
+}
---
+++ src/test/java/uz/algo/near/NearApplicationTests.java
@@ -0,0 +1,16 @@
+package uz.algo.near;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NearApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
