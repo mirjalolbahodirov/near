2ad54e89bd3d16d63b732980a4c0c5d961f14305
mirjalolbahodirov <mirjalol.bahodirov@gmail.com>
2017-12-07 14:41:43 +0500

    Facebook redirect uri fixed 2

--- src/main/java/uz/algo/near/configs/WebSecurityConfig.java
+++ src/main/java/uz/algo/near/configs/WebSecurityConfig.java
@@ -1,12 +1,17 @@
 package uz.algo.near.configs;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.connect.web.ConnectController;
 import org.springframework.social.security.SpringSocialConfigurer;
 import uz.algo.near.security.CustomAuthenticationProvider;
 import uz.algo.near.security.CustomUserDetailService;
@@ -17,6 +22,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private CustomAuthenticationProvider authenticationProvider;
+    @Autowired
+    private ConnectionFactoryLocator connectionFactoryLocator;
+    @Autowired
+    private ConnectionRepository connectionRepository;
+
+    @Value("${application.url}")
+    private String applicationUrl;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -37,13 +49,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutRequestMatcher(new AntPathRequestMatcher("/signout", "GET"))
                 .permitAll()
                 .and()
-                .apply(new SpringSocialConfigurer().alwaysUsePostLoginUrl(true));
+                .apply(new SpringSocialConfigurer());
         http.csrf().disable();
     }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authenticationProvider);
+    }
+
+    @Bean
+    public ConnectController connectController() {
+        ConnectController controller = new ConnectController(
+                connectionFactoryLocator, connectionRepository);
+        controller.setApplicationUrl(applicationUrl);
+        return controller;
     }
 
 }
